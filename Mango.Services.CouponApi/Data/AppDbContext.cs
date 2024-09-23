227f458e01748feb0d86e550e14bce30938cc3e4
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-23 23:54:17 +0300

    initial commit

---
+++ Mango.Services.CouponApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+namespace Mango.Services.CouponApi.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        /*The DbContextOptions parameter is used to configure the context, including things like the database provider (e.g., SQL Server, SQLite) and the connection string.
+This allows for configuration through Dependency Injection (DI) in ASP.NET Core, which is essential for setting up the context properly when the application starts.*/
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext > options) :base(options)
+        {
+            
+        }
+    }
+}
